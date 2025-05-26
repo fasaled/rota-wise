cef9de8797d66fb0d6201c9f778a3bed1fb3cbb6
Francisco Sánchez <fa.sl@live.com>
2025-05-26 16:12:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: React is not defined. Error source: src/components/equischedule/data-input-form.tsx (66:3) @ DataInputForm
    >
    >   64 |   const numberOfDoctors = form.watch('numberOfDoctors');
    >   65 |
    > > 66 |   React.useEffect(() => {
    >      |   ^
    >   67 |     const currentDoctorCount = fields.length;
    >   68 |     if (numberOfDoctors > currentDoctorCount) {
    >   69 |       for (let i = 0; i < numberOfDoctors - currentDoctorCount; i++) {
    >
    > Call Stack
    > 3
    >
    > Show 1 ignore-listed frame(s)
    > DataInputForm
    > src/components/equischedule/data-input-form.tsx (66:3)
    > EquiSchedulePage
    > src/app/page.tsx (137:9)

--- src/components/equischedule/data-input-form.tsx
+++ src/components/equischedule/data-input-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type * as React from 'react';
+import * as React from 'react';
 import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
