70b2f6a3a4bcace42e38303d3f1830369694d27a
Francisco Sánchez <fa.sl@live.com>
2025-06-02 19:54:32 +0000

    I mean the logo on the left of RotaWise

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { Plane, Star, Briefcase, CalendarDays, Users, Settings, Palette, CalendarX, Clock3 } from 'lucide-react';
+import { Plane, Star, Briefcase, CalendarDays, Users, Settings, Palette, CalendarX, Clock3, CalendarCheck } from 'lucide-react';
 
 export const VacationIcon = (props: React.ComponentProps<typeof Plane>) => <Plane {...props} />;
 export const PreAssignedIcon = (props: React.ComponentProps<typeof Star>) => <Star {...props} />;
@@ -9,6 +9,7 @@ export const WorkIcon = (props: React.ComponentProps<typeof Briefcase>) => <Brie
 export const CalendarIcon = (props: React.ComponentProps<typeof CalendarDays>) => <CalendarDays {...props} />;
 export const DoctorsIcon = (props: React.ComponentProps<typeof Users>) => <Users {...props} />;
 export const PreferencesIcon = (props: React.ComponentProps<typeof Settings>) => <Settings {...props} />;
-export const ThemeIcon = (props: React.ComponentProps<typeof Palette>) => <Palette {...props} />;
+export const ThemeIcon = (props: React.ComponentProps<typeof CalendarCheck>) => <CalendarCheck {...props} />;
 export const CalendarXIcon = (props: React.ComponentProps<typeof CalendarX>) => <CalendarX {...props} />;
 export const Clock3Icon = (props: React.ComponentProps<typeof Clock3>) => <Clock3 {...props} />;
+
